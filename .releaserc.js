dac5eb36e13aae31af7355cb43119d323f8a6d88
Sean Trane <seantrane@users.noreply.github.com>
2018-11-18 19:11:04 -0500

    ci(release): clean up config

--- .releaserc.js
+++ .releaserc.js
@@ -1,35 +1,27 @@
-const hooks = require('semantic-release-monorepo-hooks');
-const output = hooks();
-
-const publish = output.isLastModified
-  ? [
-    {
-      path: '@semantic-release/exec',
-      cmd: 'echo "Execute publish/deploy commands and scripts"'
-    },
-    '@semantic-release/github'
-  ]
-  : [
-    '@semantic-release/github'
-  ];
+/**
+ * Release configuration for the monorepo
+ *
+ * For more info;
+ * - https://github.com/Updater/semantic-release-monorepo
+ * - https://github.com/semantic-release/semantic-release
+ * - https://github.com/lerna/lerna
+ */
 
 module.exports = {
   branch: 'master',
-  tagFormat: 'v${version}',
+  // tagFormat: 'v${version}',
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  },
   /**
-   * Reduce expensive network calls (50%+ runtime reduction)
-   * By default, semantic-release's verifyConditions plugin configuration contains
-   * @semantic-release/npm and @semantic-release/github. These two plugins each make
-   * a network call to verify that credentials for the respective services are properly
-   * configured. When running in a monorepo, these verifications will be redundantly
-   * repeated for each and every package, greatly contributing to overall runtime.
-   * Optimally, we'd only want make these verification calls one time.
-   *
-   * By moving these plugins to the verifyRelease configuration, they will only run
-   * if semantic-release determines a release is to be made for a given package
-   * (at a time when the given verifications are actually relevant).
-   * Likely, most times semantic-release is run over a monorepo, only a small subset
-   * of all packages trigger releases.
+   * Move plugins from verifyConditions to verifyRelease to
+   * reduce expensive network calls (50%+ runtime reduction).
+   * https://github.com/Updater/semantic-release-monorepo#reduce-expensive-network-calls-50-runtime-reduction
    */
   verifyConditions: [],
   verifyRelease: [
@@ -46,14 +38,23 @@ module.exports = {
       changelogTitle: '# CHANGELOG'
     },
     '@semantic-release/npm',
-    '@semantic-release/git'
+    {
+      'path': '@semantic-release/git',
+      'message': 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}'
+    }
   ],
-  publish: publish,
+  publish: [
+    // {
+    //   path: '@semantic-release/exec',
+    //   cmd: 'echo "Execute publish/deploy commands and scripts"'
+    // },
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
   success: [
     '@semantic-release/github'
   ],
   fail: [
     '@semantic-release/github'
-  ],
-  npmPublish: false
+  ]
 };
